f7f0714c5d9d20a8906776ff7e7ee24ddf2457de
Semelit Tymofii <semelittimofij@gmail.com>
2022-01-05 23:22:47 +0200

    reloading of perf. tab added, design changes, optimization of code, README.md changes

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 PCInfo
 ======
 
-> Program created to learn new things about your system
+Program created to learn new things about your system
+
+> Program's abilities
+>> Showing info about RAM, central processor, video card, montor etc.
+>> Monitoring performance of your PC/laptop
+>> CPU speed test
--- app_class.py
+++ app_class.py
@@ -4,9 +4,10 @@ import tkinter as tk
 import tkinter.ttk as ttk
 from psutil import virtual_memory, pids, cpu_percent
 from cpuinfo import get_cpu_info
-from platform import architecture, win32_ver, system
+from platform import architecture, system
 from win32api import EnumDisplayDevices, EnumDisplaySettings, GetSystemMetrics
 from datetime import datetime
+from math import pi as p
 
 # Class
 
@@ -31,38 +32,75 @@ class App():
 
 		self.drawWidgets()
 
+	'''Optimization. DRY(don\'t repeat yourself).
+	These variables are used often.
+	Code becomes more readable'''
+
+	def setPerfVariables(self):
+
+		self.available_ram = round(self.ram_info.available/1024/1024/1024, 3)
+		self.ram_usage_percent = round(self.ram_info.percent)
+		self.used_ram = self.ram_amount * (self.ram_usage_percent/100)
+
+		self.processes_number = len(pids())
+		self.cpu_usage_percent = cpu_percent()
+
+	'''Ability of reloading the information in performance tab'''
+
+	def reloadFrames(self):
+
+		self.setPerfVariables()
+
+		self.available_ram_lbl.config(text=f'Available RAM(GB): {self.available_ram}')
+		self.p_ram_usage_lbl.config(text=f'RAM usage percent(%): {self.ram_usage_percent}')
+		self.ram_usage_lbl.config(text=f'RAM usage(GB): {self.used_ram}')
+
+		self.proc_num_lbl.config(text=f'Processes amount: {self.processes_number}')
+		self.cpu_usage_num_lbl.config(text=f'CPU usage percent(%): {self.cpu_usage_percent}')
+
 	def calculateSpeed(self):
 
 		'''CPU Speed test. The speed will be known by calculating number π'''
 
 		start_time = datetime.now().microsecond
-		pi = 11 / 3.5
-		self.pi_label.config(text=f'π: {pi}')
+
+		11 / 3.5
+
+		self.pi_label.config(text=f'π: {p}')
 		self.result = datetime.now().microsecond - start_time
+
 		while self.result == 0:
+
 			start_time = datetime.now().microsecond
-			pi = 11 / 3.5
+
+			11 / 3.5
+
 			self.result = datetime.now().microsecond - start_time
+
 		self.result_label.config(text='Result: ' + str(self.result) + ' ms')
 
 	def drawLabels(self, parent, var, text, row=None, additional_str='', font_size=12):
-		
+
 		'''It is made to stop reapiting in code.
 		Labels with information about system are used very often.
 		Method is a "template" to create them'''
-		
+
 		label = tk.Label(parent,
 						 text=f'{text}{additional_str}{var}',
 						 font=('Consolas', font_size),
 						 padx=10,
-						 pady=10)
+						 pady=10,
+						 bg='#2C2C2C',
+                         fg='#62CA00')
 		label.grid(row=row, column=0, sticky=tk.W)
+
+		return label
 
 	def drawWidgets(self):
 
 		'''The main part. Drawing 3 tabs and system's characteristics labels for each'''
 
-		ram_info = virtual_memory()
+		self.ram_info = virtual_memory()
 
 		row = 0
 
@@ -72,51 +110,47 @@ class App():
 
 		# Computer hardware tab and its widgets
 
-		computer_hardware_tab = tk.Frame(tabs)
+		computer_hardware_tab = tk.Frame(tabs, bg='#2C2C2C')
 
 		# Variables
 
 		cpu = get_cpu_info()['brand_raw']
 
-		ram_amount = round(ram_info.total/1024/1024/1000)
+		self.ram_amount = round(self.ram_info.total/1024/1024/1000)
 
 		system_bitness = architecture()[0]
 
 		# Widgets
 
-		if os == 'Windows':
-
-			windows_version = win32_ver()[0]
-
-			self.drawLabels(parent=computer_hardware_tab,
-								var=' '+windows_version,
-								text='Windows version: ',
-								row=row,
-								additional_str='Windows')
-			row += 1
-
 		self.drawLabels(parent=computer_hardware_tab,
-							var=ram_amount,
-							text='RAM(Gb): ',
-							row=row)
+						var=os,
+						text='OS: ',
+						row=row)
+
 		row += 1
 
 		self.drawLabels(parent=computer_hardware_tab,
-							var=cpu,
-							text='CPU: ',
-							row=row)
+						var=self.ram_amount,
+						text='RAM(Gb): ',
+						row=row)
 		row += 1
 
 		self.drawLabels(parent=computer_hardware_tab,
-							var=system_bitness,
-							text='System bitness: ',
-							row=row)
+						var=cpu,
+						text='CPU: ',
+						row=row)
+		row += 1
+
+		self.drawLabels(parent=computer_hardware_tab,
+						var=system_bitness,
+						text='System bitness: ',
+						row=row)
 
 		tabs.add(computer_hardware_tab, text='Computer hardware')
 
 		# Monitor characteristics tab and its widgets
 
-		monitor_characteristics_tab = tk.Frame(tabs)
+		monitor_characteristics_tab = tk.Frame(tabs, bg='#2C2C2C')
 
 		# Variables
 
@@ -129,57 +163,81 @@ class App():
 		# Widgets
 
 		self.drawLabels(parent=monitor_characteristics_tab,
-							var=vid_card,
-							text='Video card: ',
-							row=0)
+						var=vid_card,
+						text='Video card: ',
+						row=0)
 
 		self.drawLabels(parent=monitor_characteristics_tab,
-							var=freq,
-							text='Frequency(GHz): ',
-							row=1)
+						var=freq,
+						text='Frequency(GHz): ',
+						row=1)
 
 		self.drawLabels(parent=monitor_characteristics_tab,
-							var=screen_resolution,
-							text='Screen resolution: ',
-							row=2)
+						var=screen_resolution,
+						text='Screen resolution: ',
+						row=2)
 
 		tabs.add(monitor_characteristics_tab, text='Monitor')
 
 		# Performance tab and its widgets
 
-		performance_tab = tk.Frame(tabs)
+		performance_tab = tk.Frame(tabs, bg='#2C2C2C')
 
 		# Variables
 
-		available_ram = round(ram_info.available/1024/1024/1024, 2)
-		ram_usage_percent = round(ram_info.percent)
-		used_ram = ram_amount * (ram_usage_percent/100)
-
-		processes_number = len(pids())
-		cpu_usage_percent = cpu_percent()
+		self.setPerfVariables()
 
 		# Widgets
 
 		# RAM frame
 
-		ram_frame = tk.LabelFrame(performance_tab, text='Physical memory')
+		ram_frame = tk.LabelFrame(performance_tab, text='Physical memory', bg='#2C2C2C', fg='#fff')
 
-		self.drawLabels(parent=ram_frame, var=available_ram, text='Available RAM(GB): ', row=0, font_size=8)
+		self.available_ram_lbl = self.drawLabels(parent=ram_frame,
+												 var=self.available_ram,
+												 text='Available RAM(GB): ',
+												 row=0,
+												 font_size=8)
 
-		self.drawLabels(parent=ram_frame, var=ram_usage_percent, text='RAM usage percent(%): ', row=1, font_size=8)
+		self.p_ram_usage_lbl = self.drawLabels(parent=ram_frame,
+											   var=self.ram_usage_percent,
+											   text='RAM usage percent(%): ',
+											   row=1,
+											   font_size=8)
 
-		self.drawLabels(parent=ram_frame, var=used_ram, text='RAM usage(GB): ', row=2, font_size=8)
+		self.ram_usage_lbl = self.drawLabels(parent=ram_frame,
+											 var=self.used_ram,
+											 text='RAM usage(GB): ',
+											 row=2,
+											 font_size=8)
 
 		ram_frame.grid(row=0, column=0, padx=5, pady=5)
 
 		# System frame
 
-		system_frame = tk.LabelFrame(performance_tab, text='System')
+		system_frame = tk.LabelFrame(performance_tab,
+									 text='System',
+									 bg='#2C2C2C',
+									 fg='#fff')
 
-		self.drawLabels(parent=system_frame, var=processes_number, text='Processes: ', row=0, font_size=8)
-		self.drawLabels(parent=system_frame, var=cpu_usage_percent, text='CPU usage percent(%): ', row=1, font_size=8)
+		self.proc_num_lbl = self.drawLabels(parent=system_frame,
+											var=self.processes_number,
+											text='Processes amount: ',
+											row=0,
+											font_size=8)
+		self.cpu_usage_num_lbl = self.drawLabels(parent=system_frame,
+												 var=self.cpu_usage_percent,
+												 text='CPU usage percent(%): ',
+												 row=1,
+												 font_size=8)
 
 		system_frame.grid(row=0, column=2)
+
+		reload_btn = tk.Button(performance_tab,
+							   text='Reload',
+							   font=('Consolas', 14),
+							   command=self.reloadFrames)
+		reload_btn.grid(row=1, column=3, padx=10, pady=10)
 
 		tabs.add(performance_tab, text='Performance')
 
@@ -187,27 +245,36 @@ class App():
 
 		# CPU speed test tab and its widgets
 
-		cpu_speed_test_tab = tk.Frame(tabs)
+		cpu_speed_test_tab = tk.Frame(tabs, bg='#2C2C2C')
 
 		# Widgets
 
 		info_label = tk.Label(cpu_speed_test_tab,
 							  text='Test your processor speed with calculating π \nand showing it on the screen',
-							  font=('Consolas', 12))
+							  font=('Consolas', 12),
+							  bg='#2C2C2C',
+							  fg='#20B2AA')
 		info_label.grid(row=0, column=0, sticky=tk.W, padx=10, pady=10)
 
 		self.result_label = tk.Label(cpu_speed_test_tab,
 							  text='Result: ',
-							  font=('Consolas', 12))
+							  font=('Consolas', 12),
+							  bg='#2C2C2C',
+							  fg='#F43416')
 		self.result_label.grid(row=1, column=0, sticky=tk.W, padx=10, pady=10)
 
 		self.pi_label = tk.Label(cpu_speed_test_tab,
 							  text='π: ',
-							  font=('Consolas', 12))
+							  font=('Consolas', 12),
+							  bg='#2C2C2C',
+							  fg='#62CA00')
 		self.pi_label.grid(row=2, column=0, sticky=tk.W, padx=10, pady=10)
 
-		start_btn = tk.Button(cpu_speed_test_tab, text='Start', font=('Consolas', 14), command=self.calculateSpeed)
-		start_btn.grid(row=3, column=0, padx=10)
+		start_btn = tk.Button(cpu_speed_test_tab,
+							  text='Start',
+							  font=('Consolas', 14),
+							  command=self.calculateSpeed)
+		start_btn.grid(row=3, column=0, padx=10, pady=10)
 
 		tabs.add(cpu_speed_test_tab, text='CPU Speed test')
 
